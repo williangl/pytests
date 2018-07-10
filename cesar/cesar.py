8c0698f96381c670951b26feee5e0a026f3c9d28
williangl <willian_gl@hotmail.com>
2018-07-10 17:35:45 -0300

    Cesar com teste

---
+++ cesar/cesar.py
@@ -0,0 +1,28 @@
+"""Cifra de Cesar."""
+
+# biblioteca padrão
+from string import ascii_lowercase
+
+alf = ascii_lowercase
+
+txt1 = 'abc'
+txt2 = 'xyz'
+
+
+def cripto(txt1: str) -> str:
+    """Função de criptografia."""
+    cipher1 = []
+    for letra1 in txt1:
+        cipher1.append(alf[(alf.index(letra1) + 3) % len(alf)])
+    return ''.join(cipher1)
+
+
+"""
+def xyz(txt: str) -> str:
+
+    cipher2 = []
+    for letra2 in txt2:
+        cipher2.append(alf[(alf.index(letra2) + 3) % len(alf)])
+    return ''.join(cipher2)
+    return ''.join(cipher2)
+"""
---
+++ cesar/cesartest.py
@@ -0,0 +1,26 @@
+"""Teste cifra de Cesar."""
+
+# biblioteca padrão
+from unittest import TestCase, main
+
+# minha biblioteca
+import cesar
+
+
+class TesteCesar(TestCase):
+    """Classe de teste."""
+
+    def test_cripto(self):
+        """Testar a função de criptografia."""
+        entrada = 'abc'
+        esperado = 'def'
+        self.assertEqual(cesar.cripto(entrada), esperado)
+
+    def test_xyz(self):
+        """Testar criptografia com as últimas letras do alfabeto."""
+        entrada = 'xyz'
+        esperado = 'abc'
+        self.assertEqual(cesar.cripto(entrada), esperado)
+
+
+main()
